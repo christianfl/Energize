a90cfc2d9a319d5c2b168784cb44bf42e1ed7f82
Christian <epinez@noreply.codeberg.org>
2022-03-27 15:02:59 +0200

    Pressing back on other tabs than tracking returns to tracking instead of closing the app, closes #15

--- lib/pages/tabs_page.dart
+++ lib/pages/tabs_page.dart
@@ -18,23 +18,35 @@ class _TabsPageState extends State<TabsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: _currentIndex,
-        children: [
-          for (final tabItem in TabNavItem.getItems(context)) tabItem.page,
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (int index) => _setIndex(index),
-        items: [
-          for (final tabItem in TabNavItem.getItems(context))
-            BottomNavigationBarItem(
-              icon: tabItem.icon,
-              label: tabItem.label,
-            )
-        ],
+    return WillPopScope(
+      onWillPop: () async {
+        if (_currentIndex == 0) {
+          return true;
+        } else {
+          setState(() {
+            _currentIndex = 0;
+          });
+          return false;
+        }
+      },
+      child: Scaffold(
+        body: IndexedStack(
+          index: _currentIndex,
+          children: [
+            for (final tabItem in TabNavItem.getItems(context)) tabItem.page,
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (int index) => _setIndex(index),
+          items: [
+            for (final tabItem in TabNavItem.getItems(context))
+              BottomNavigationBarItem(
+                icon: tabItem.icon,
+                label: tabItem.label,
+              )
+          ],
+        ),
       ),
     );
   }
