a7933ed7505b57990f7e64393f30dc3c5448f3f4
Christian <epinez@noreply.codeberg.org>
2022-07-09 13:56:55 +0200

    Updated version to v0.5.0

--- lib/services/food_database_bindings/open_food_facts/open_food_facts_binding.dart
+++ lib/services/food_database_bindings/open_food_facts/open_food_facts_binding.dart
@@ -19,7 +19,7 @@ class OpenFoodFactsBinding {
   OpenFoodFactsBinding._privateConstructor() {
     OpenFoodAPIConfiguration.userAgent = const UserAgent(
       name: 'Energize',
-      version: '0.4.0',
+      version: '0.5.0',
       url: 'https://codeberg.org/epinez/Energize',
     );
   }
