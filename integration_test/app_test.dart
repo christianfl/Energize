f96c9183c0f4537dac70673055fb704135c16017
Christian <epinez@noreply.codeberg.org>
2024-08-13 22:45:01 +0200

    Improve integration tests

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,11 +1,16 @@
 import 'dart:io';
 
 import 'package:energize/main.dart';
+import 'package:energize/models/food/food_tracked.dart';
 import 'package:energize/pages/tab_food/food_page.dart';
 import 'package:energize/pages/tab_settings/settings_page.dart';
+import 'package:energize/pages/tab_tracking/tracking_page.dart';
+import 'package:energize/providers/tracked_food_provider.dart';
+import 'package:energize/services/food_database_bindings/swiss_food_composition_database/swiss_food_composition_database_binding.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   // Setup
@@ -25,11 +30,7 @@ void main() {
   }
 
   group('Tracking Page Test', () {
-    // Nothing here yet
-  });
-
-  group('Custom Food Page Test', () {
-    testWidgets('isLoaded', (WidgetTester tester) async {
+    testWidgets('Tracking Page Screenshots', (WidgetTester tester) async {
       // Load MyApp widget
       await tester.pumpWidget(const MyApp(debugShowCheckedModeBanner: false));
 
@@ -40,7 +41,125 @@ void main() {
       // See: https://github.com/flutter/flutter/issues/128100#issuecomment-1576334061
       await binding.convertFlutterSurfaceToImage();
 
-      /// Check there are three navigation bar items
+      // Check whether TrackingPage is loaded
+      expect(find.byType(TrackingPage), findsOneWidget);
+
+      // Get BuildContext
+      final BuildContext context = tester.element(find.byType(TrackingPage));
+
+      // Prepare tracked Food
+      final now = DateTime.now();
+
+      // Prepare food from Swiss Food Composition Database
+      final sfcdFoods = SwissFoodCompositionDatabaseBinding.allFoods;
+      final avocado =
+          sfcdFoods.firstWhere((elem) => elem.title.startsWith('Avocado'));
+      final banana =
+          sfcdFoods.firstWhere((elem) => elem.title.startsWith('Banane'));
+      final apple =
+          sfcdFoods.firstWhere((elem) => elem.title.startsWith('Apfel, roh'));
+      final oatmeal =
+          sfcdFoods.firstWhere((elem) => elem.title.startsWith('Haferflocken'));
+      final coconutMilk = sfcdFoods
+          .firstWhere((elem) => elem.title.startsWith('Kokosnussmilch'));
+
+      final trackedAvocado = FoodTracked.fromFood(
+        avocado,
+        FoodTracked.generatedId,
+        120,
+        now,
+        now,
+      );
+      final trackedBanana = FoodTracked.fromFood(
+        banana,
+        FoodTracked.generatedId,
+        90,
+        now,
+        now,
+      );
+      final trackedApple = FoodTracked.fromFood(
+        apple,
+        FoodTracked.generatedId,
+        180,
+        now,
+        now,
+      );
+      final trackedOatMeal = FoodTracked.fromFood(
+        oatmeal,
+        FoodTracked.generatedId,
+        100,
+        now,
+        now,
+      );
+      final trackedCoconutMilk = FoodTracked.fromFood(
+        coconutMilk,
+        FoodTracked.generatedId,
+        350,
+        now,
+        now,
+      );
+
+      // Add tracked food
+      final trackedFoodProvider =
+          Provider.of<TrackedFoodProvider>(context, listen: false);
+      trackedFoodProvider.addEatenFood(trackedAvocado);
+      trackedFoodProvider.addEatenFood(trackedBanana);
+      trackedFoodProvider.addEatenFood(trackedApple);
+      trackedFoodProvider.addEatenFood(trackedOatMeal);
+      trackedFoodProvider.addEatenFood(trackedCoconutMilk);
+
+      // Wait until all frames were drawn
+      await tester.pumpAndSettle();
+
+      // Create and save a screenshot
+      await takeAndroidScreenshot(
+        'en-US/images/phoneScreenshots/test_1',
+        tester,
+      );
+
+      // Go to detailed view next...
+
+      // Check there is a PopupMenuButton
+      final button = find.byType(PopupMenuButton<int>);
+      expect(button, findsOne);
+
+      // Emulate a tap on the PopupMenuButton
+      await tester.tap(button);
+
+      // Wait until all frames were drawn
+      await tester.pumpAndSettle();
+
+      // Check there are at least two menu items
+      final menuItems = find.byType(PopupMenuItem<int>);
+      expect(menuItems, findsAtLeast(2));
+
+      // Emulate a tap on the first PopupMenuButton (detailed summary)
+      await tester.tap(menuItems.first);
+
+      // Wait until all frames were drawn
+      await tester.pumpAndSettle();
+
+      // Create and save a screenshot
+      await takeAndroidScreenshot(
+        'en-US/images/phoneScreenshots/test_2',
+        tester,
+      );
+    });
+  });
+
+  group('Custom Food Page Test', () {
+    testWidgets('Custom Food Page Screenshot', (WidgetTester tester) async {
+      // Load MyApp widget
+      await tester.pumpWidget(const MyApp(debugShowCheckedModeBanner: false));
+
+      // Wait until all frames were drawn
+      await tester.pumpAndSettle();
+
+      // This has to be present after tester.pumpAndSettle()
+      // See: https://github.com/flutter/flutter/issues/128100#issuecomment-1576334061
+      await binding.convertFlutterSurfaceToImage();
+
+      // Check there are three navigation bar items
       final navigationItems = find.byType(NavigationDestination);
       expect(navigationItems, findsExactly(3));
 
@@ -62,7 +181,7 @@ void main() {
   });
 
   group('Settings Page Test', () {
-    testWidgets('isLoaded', (WidgetTester tester) async {
+    testWidgets('Settings Page Screenshot', (WidgetTester tester) async {
       // Load MyApp widget
       await tester.pumpWidget(const MyApp(debugShowCheckedModeBanner: false));
 
@@ -73,7 +192,7 @@ void main() {
       // See: https://github.com/flutter/flutter/issues/128100#issuecomment-1576334061
       await binding.convertFlutterSurfaceToImage();
 
-      /// Check there are three navigation bar items
+      // Check there are three navigation bar items
       final navigationItems = find.byType(NavigationDestination);
       expect(navigationItems, findsExactly(3));
 
